9b8cf27f024e24534fe2284c0881c757d4874f97
andrei.ursaki <shatter007@mail.ru>
2020-02-21 13:51:31 +0300

    --

--- config.py
+++ config.py
@@ -5,6 +5,6 @@ import redis
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'this-is_super-hard-secret_key'
-    hour_for_update = 10
+    hour_for_update = 14
     # conn_local = redis.Redis(db=1)
     conn_heroku = redis.from_url(os.environ.get("REDIS_URL"))
