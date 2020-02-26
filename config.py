15f696981527826b6a418325e0c61a1dab2e78ab
andrei.ursaki <shatter007@mail.ru>
2020-02-26 11:09:23 +0300

    --

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ import redis
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'this-is_super-hard-secret_key'
     hour_for_update = 11
-    minute_for_update = 0
+    minute_for_update = 15
     try:
         conn = redis.from_url(os.environ.get("REDIS_URL"))
         hour_for_update = hour_for_update - 3
